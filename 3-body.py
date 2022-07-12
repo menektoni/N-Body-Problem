7d14a3d56c8a65759953631c6c756c0ea2696e0b
Toni Domenech Borrell <tonidome@gmail.com>
2022-07-12 23:42:12 +0200

    Class created with gravitational field calculation already done.

--- 3-body.py
+++ 3-body.py
@@ -1,3 +1,13 @@
 import numpy as np
 import scipy as sp
 
+from scipy import constants
+
+class oneBody:
+    def __init__(self, mass):
+        self.mass = mass
+
+    def gravitational_field(x, y):
+        modulus = np.sqrt(x**2 + y**2)
+        g = -sp.constants.physical_constants[G]*self.mass/modulus**2*[x/modulus, y/modulus]
+        return g
