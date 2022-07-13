035bcd6f1f826e0d8d4c8600b6bd8e8eb91fa40a
Toni Domenech Borrell <tonidome@gmail.com>
2022-07-13 18:25:25 +0200

    Minor updates + debugging. Familiarizing with some class aspects.

--- 3-body.py
+++ 3-body.py
@@ -7,7 +7,13 @@ class oneBody:
     def __init__(self, mass):
         self.mass = mass
 
-    def gravitational_field(x, y):
+    def gravitational_field(self, x, y):
         modulus = np.sqrt(x**2 + y**2)
-        g = -sp.constants.physical_constants[G]*self.mass/modulus**2*[x/modulus, y/modulus]
+        g_x = -constants.G*self.mass/(modulus**2)*x/modulus
+        g_y = -constants.G*self.mass/(modulus**2)*y/modulus
+        g = [g_x, g_y]
         return g
+
+    def get_mass(self):
+        return self.mass
+
